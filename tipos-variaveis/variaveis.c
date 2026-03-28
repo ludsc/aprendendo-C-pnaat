93de924283c75160f78347897e4cf2fece9291c6
lucas-C2025 <santo.carvalho02@gmail.com>
2026-03-28 17:40:51 -0300

    Add variable declarations for industrial conveyor system simulation

---
+++ tipos-variaveis/variaveis.c
@@ -0,0 +1,11 @@
+// Criando variaveis para uma situação de uma esteira industrial
+int conta_caixa; // escolhido porque as caixas só podem ser inteiros
+
+float velocidade; //por exatidão é interessante que seja um float
+
+char tecla; // por questao de armazenamento é interessante que seja apenas um char
+
+int tempo_operacao; // escolhido por questão de otimização
+
+unsigned long long int tempo_operacao; // apenas como exemplo de situação de uso de um tipo de variavel muito longo
+
